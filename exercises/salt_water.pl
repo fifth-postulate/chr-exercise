48955753ff48eced0bb657dfeb3f5c7303d99a25
Daan van Berkel <daan.van.berkel@alliander.com>
2020-06-03 13:47:25 +0200

    recreate salt_water exercise

---
+++ exercises/salt_water.pl
@@ -0,0 +1,13 @@
+use_module(library(chr)).
+
+/*
+ *  CHR syntax, since Anne can never remember it, and probably I can't either:
+ *  name @ retained \ discarded <=> guard | head,body.    Simpagation
+ *  name @ discarded <=> guard | head, body.              Simplification
+ *  name @ retained ==> guard | head, body.               Propagation
+ *
+*/
+
+:- chr_constraint salt/0, water/0, salt_water/0.
+
+salt,water <=> salt_water.
